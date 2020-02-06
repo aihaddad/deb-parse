6cc4abc5c011cf0b3e689f4b0e169c6686acf0f5
Ahmed Elhaddad <aihaddad@outlook.com>
2020-02-06 19:03:19 +0200

    Lay out DebianControlFileParser class

---
+++ dpkgcf-parser/parser.py
@@ -0,0 +1,20 @@
+# TODO
+class DebianControlFileParser:
+    # TODO
+    def __init__(self, file):
+        pass
+
+    # TODO
+    def to_json(self, output="datastore/debian-packages.json"):
+        pass
+
+
+    # Private
+    # TODO
+    def __get_raw_info(self, text):
+        pass
+
+
+    # TODO
+    def __get_useful_info(self, pkg_raw_info):
+        pass
